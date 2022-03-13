9f6c904c0e2fbf483443ade2f7a515b2abe9536a
Kevin <kevin.j.wilkerson@gmail.com>
2022-03-13 17:44:58 -0500

    added validators

--- models/Comment.js
+++ models/Comment.js
@@ -10,9 +10,12 @@ const ReplySchema = new Schema(
     },
     replyBody: {
       type: String,
+      required: "You need to enter a reply!",
+      trim: true,
     },
     writtenBy: {
       type: String,
+      required: "You need to enter your name to post a reply!",
     },
     createdAt: {
       type: Date,
@@ -31,9 +34,13 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: "You need to enter your name to post a comment!",
+      trim: true,
     },
     commentBody: {
       type: String,
+      required: "You need to enter a comment",
+      trim: true,
     },
     createdAt: {
       type: Date,
